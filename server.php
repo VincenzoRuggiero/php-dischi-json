b7fc98880a883e5f2a2776d4f4fd6fe12f97f04a
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-30 15:42:22 +0100

    Add server condition for empty ajax responses

--- server.php
+++ server.php
@@ -2,6 +2,10 @@
     $jsonData = file_get_contents(__DIR__ . "/data.json");
     $discList = json_decode($jsonData, true);
 
+    if(is_null($discList)){
+        $discList = [];
+    }
+
     header('Content-Type: application/json');
 
     echo json_encode($discList);
